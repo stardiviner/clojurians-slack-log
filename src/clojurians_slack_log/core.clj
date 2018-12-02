ce92396b21124405bd817678dc18b09f697d92e9
stardiviner <numbchild@gmail.com>
2018-12-02 19:22:35 +0800

    initialize

---
+++ src/clojurians_slack_log/core.clj
@@ -0,0 +1,6 @@
+(ns clojurians-slack-log.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/clojurians_slack_log/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojurians-slack-log.core-test
+  (:require [clojure.test :refer :all]
+            [clojurians-slack-log.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
